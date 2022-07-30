47cc3adb2e0f4814c0a75755489ef78a0e9209ce
BogdanD123 <108362027+BogdanD123@users.noreply.github.com>
2022-07-30 21:02:20 +0200

    first commit

---
+++ Practice/test.js
@@ -0,0 +1,36 @@
+// let num = 9 % 3 == 0;
+
+// let number = 2 * 3;
+// console.log(number / 2);
+
+// let faktoriel = 3 * 2;
+
+// let num = 17;
+// if (num % 2 === 0 && num > 15) {
+//   console.log("deliv i pogolem od 15");
+// } else {
+//   console.log("ne e deliv so 2 i pogolem od 15");
+// }
+// let num = 18;
+
+// let number = 25;
+// let nameBojan = "bojan";
+// if (number) {
+//   nameBojan = nameBojan + number;
+// }
+// if (!number) {
+//   number * 2;
+// }
+
+// console.log(number)
+
+// let word = avg;
+// let bool = false
+// let ime = ime
+// if()
+
+// console.log(avg + false + ime)
+
+// let num = "";
+// let num2 = 2;
+// let num3 = "3"
