375a078987617bc834012c1c38abea4e3db850b8
BogdanD123 <108362027+BogdanD123@users.noreply.github.com>
2022-08-16 01:49:38 +0200

    first commit

---
+++ javascriptFile2.js
@@ -0,0 +1,26 @@
+// let value = false;
+// alert(typeof value); //.This type is Boolean
+
+// value = String(value);
+// alert(typeof value);
+
+// let value = "false";
+// alert(typeof value); //.This type is String
+
+// value = Boolean(value);
+// alert(typeof value);
+
+// let num1 = 15;
+
+// console.log(++num1);
+
+let number = 15;
+
+// console.log(number % 6);
+let fisrtNumber = 15;
+let secondNumber = "15";
+let thirdNumber = "1234";
+let stringNumber = Number(secondNumber);
+
+let rezultat = 10 != Number(thirdNumber / secondNumber);
+console.log(rezultat);
