ed987622643b4eb5d2724143c797875b44e0dbe2
BogdanD123 <108362027+BogdanD123@users.noreply.github.com>
2022-08-26 01:08:13 +0200

    first commit

---
+++ javascriptfile/javalooparr.js
@@ -0,0 +1,35 @@
+let arr = [1221, 2333, 3222, 411, 545, 6];
+let splicedNumber = 2333;
+function removeNumber(array, number) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == number) {
+      console.log(arr, splicedNumber);
+      array.splice(i, 1);
+    }
+  }
+  return array;
+}
+console.log(removeNumber(arr, splicedNumber));
+
+let niza = [231, 21111, 2333, 22];
+let number = 0;
+for (let i = 0; i < niza.length; i++) {
+  if (number < niza[i]) {
+    number = niza[i];
+  }
+}
+
+console.log(number);
+const number1 = 1112211;
+
+while (number1 > 0) {
+  const digits = (number1, count = 0) => {
+    if (number1) {
+      return digits(Math.floor(number1 / 10), ++count);
+    }
+    return count;
+  };
+  console.log(digits(number1));
+
+  break;
+}
